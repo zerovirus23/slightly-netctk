56c929830e1cb3455cd49aac2a58f2853a795d6b
Hernan Tenjo <zerovirus23@gmail.com>
2017-05-17 23:56:59 -0500

    Adding Lombok to reduce boilerplate code and first approach to the parser solution.

---
+++ src/main/java/biz/netcentric/engine/Template.java
@@ -0,0 +1,10 @@
+package biz.netcentric.engine;
+
+/**
+ * Created by hernan on 5/17/17.
+ */
+public class Template {
+    public static String compile(TemplateContext context){
+        return null;
+    }
+}
